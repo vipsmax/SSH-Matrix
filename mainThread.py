807c59ea8a3f63279bf7dbd8e0f485890a17a53c
C'tri Goudie <43475390+Ctri-The-Third@users.noreply.github.com>
2019-08-29 17:58:39 +0100

    Update mainThread.py

--- mainThread.py
+++ mainThread.py
@@ -13,15 +13,20 @@ import SQLconnector
 
 conn = SQLconnector.connectToSource()
 cursor = conn.cursor()
-sql = "select p.PlayerID, GamerTag from participation p join players pl on p.playerID = pl.playerID order by NEWID ()"
+sql = """select p.PlayerID, GamerTag 
+from participation p 
+join players pl on p.playerID = pl.playerID 
+join InterestingPlayers ip on p.PlayerID = ip.playerID 
+where Seenin60days = 'active'
+order by NEWID ()"""
 cursor.execute(sql)
 dimensions = render.get_terminal_size_windows()
 
 os.system("cls")
 
 for row in cursor.fetchall():
-    var = MatrixLine(   row[0]+row[1],
-                        (random()*dimensions[0])-random()*len(row[0]+row[1]),
+    var = MatrixLine(   row[1],
+                        (random()*dimensions[0])-random()*len(row[1]),
                         random()*dimensions[1],
                         random()*1)
     var.start() 
